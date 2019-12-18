7db4a644264448ec411bfbaf53b2689417c4b106
J4m3s00 <james.lol2000@gmail.com>
2019-12-18 17:49:32 +0100

    Fixing spdlog

--- Infinit/src/Core/Log.cpp
+++ Infinit/src/Core/Log.cpp
@@ -1,4 +1,5 @@
 #include "inpch.h"
+#include <spdlog/sinks/stdout_sinks.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace Infinit {
@@ -11,10 +12,10 @@ namespace Infinit {
 	{
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		s_CoreLogger = spdlog::stdout_color_mt<std::shared_ptr<spdlog::logger>>("INFINIT", spdlog::color_mode::automatic);
+		s_CoreLogger = spdlog::stdout_color_mt("INFINIT", spdlog::color_mode::always);
 		s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt<std::shared_ptr<spdlog::logger>>("APP", spdlog::color_mode::automatic);
+		s_ClientLogger = spdlog::stdout_color_mt("APP", spdlog::color_mode::automatic);
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 
